b2b0b28b516aa5e9cbcc62897d55c6f4aa31640d
Gustavo Aires <gustavoairesmatos@gmail.com>
2019-09-21 17:15:50 -0300

    changing static files dir

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+process.env.PWD = process.cwd();
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/app/index.html'));
 });
 
-app.use(express.static(__dirname, '/app')); // set static files location, in this case the route, add a file name if not
+app.use(express.static(process.env.PWD, '/app')); // set static files location, in this case the route, add a file name if not
 app.listen(process.env.PORT || 3000);
