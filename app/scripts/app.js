a6e907f89e196996c83a442c8be8abf63372b8f5
Gustavo Aires <gustavoairesmatos@gmail.com>
2019-09-21 17:46:23 -0300

    config angular for prod

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,7 @@
  */
 angular
   .module('reportApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
     'ngRoute',
-    'ngSanitize',
-    'ngTouch'
   ])
   .config(function($routeProvider) {
     $routeProvider
